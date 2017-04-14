fc80e7718c1e29bd203a119c02e1a035b5cccf58
SsoftDevelep <saitemresolmaz@gmail.com>
2017-04-14 06:29:54 +0300

    Tasarimi yapip ,Veritabani kismini fake data ile hallettik

---
+++ MyEvernote.DataAccessLayer/MyInitializer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MyEvernote.Entities;
+
+namespace MyEvernote.DataAccessLayer
+{
+    public class MyInitializer : CreateDatabaseIfNotExists<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context) //ornek data basimindaki method
+        {
+            //adding admin user
+            EvernoteUser admin = new EvernoteUser()
+            {
+                Name = "semre",
+                Surname = "solmaz",
+                Email = "emresolmaz@outlook.com",
+                ActivateGuid = Guid.NewGuid(),
+                IsActive = true,
+                IsAdmin = true,
+                UserName = "emresolmaz",
+                Password = "123456",
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now.AddMinutes(5),
+                ModifiedUsername = "emresolmaz"
+
+            };
+
+            //adding standart user
+            EvernoteUser standartUser = new EvernoteUser()
+            {
+                Name = "sait",
+                Surname = "solmaz",
+                Email = "saitsolmaz@outlook.com",
+                ActivateGuid = Guid.NewGuid(),
+                IsActive = true,
+                IsAdmin = false,
+                UserName = "saitsolmaz",
+                Password = "654321",
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now.AddMinutes(5),
+                ModifiedUsername = "emresolmaz"
+
+            };
+
+            context.EvernoteUsers.Add(admin);
+            context.EvernoteUsers.Add(standartUser);
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                EvernoteUser user = new EvernoteUser()
+                {
+                    Name = FakeData.NameData.GetFirstName(),
+                    Surname = FakeData.NameData.GetSurname(),
+                    Email = FakeData.NetworkData.GetEmail(),
+                    ActivateGuid = Guid.NewGuid(),
+                    IsActive = true,
+                    IsAdmin = false,
+                    UserName = $"user{i}",   //yeni formatlama
+                    Password = "123",
+                    CreatedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                    ModifiedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                    ModifiedUsername = $"user{i}"
+
+                };
+                context.EvernoteUsers.Add(user);
+            }
+
+            context.SaveChanges();
+
+            //User list for using
+            List<EvernoteUser> userlist = context.EvernoteUsers.ToList();
+
+            //Adding fake categories..
+            for (int t = 0; t < 10; t++)
+            {
+                Category ctg = new Category()
+                {
+                    Title = FakeData.PlaceData.GetStreetName(),
+                    Description = FakeData.PlaceData.GetAddress(),
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = DateTime.Now,
+                    ModifiedUsername = "emresolmaz"
+                };
+
+                context.Categories.Add(ctg);
+
+                //Adding fake notes
+                for (int k = 0; k < FakeData.NumberData.GetNumber(5, 9); k++)
+                {
+                    EvernoteUser owner = userlist[FakeData.NumberData.GetNumber(0, userlist.Count - 1)];
+                    Note note = new Note()
+                    {
+                        Title = FakeData.TextData.GetAlphabetical(FakeData.NumberData.GetNumber(5, 25)),
+                        Text = FakeData.TextData.GetSentences(FakeData.NumberData.GetNumber(1, 3)),
+                        IsDraft = false,
+                        LikeCount = FakeData.NumberData.GetNumber(1, 9),
+                        Owner = owner,
+                        CreatedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                        ModifiedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                        ModifiedUsername = owner.UserName,
+
+                    };
+
+                    ctg.Notes.Add(note);
+
+
+                    
+                    //Adding fake comments
+                    for (int m = 0; m < FakeData.NumberData.GetNumber(1, 10); m++)
+                    {
+                        EvernoteUser comment_owner = userlist[FakeData.NumberData.GetNumber(0, userlist.Count - 1)];
+                        Comment com = new Comment()
+                        {
+                            Text = FakeData.TextData.GetSentence(),
+                            CreatedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                            ModifiedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                            ModifiedUsername = comment_owner.UserName,
+                            Owner = comment_owner,
+
+                        };
+                        note.Comments.Add(com);
+
+                        //Adding likes
+
+                        
+                        for (int j = 0; j < note.LikeCount; j++)
+                        {
+                            Liked likes = new Liked()
+                            {
+                                LikedUser = userlist[j],
+
+                            };
+
+                            note.Likes.Add(likes);
+                        }
+
+                    }
+                }
+                context.SaveChanges();
+
+            }
+            
+        }
+    }
+}
+
+
+
---
+++ MyEvernote.Entities/MyEntityBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.Entities
+{
+    public class MyEntityBase
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)] //primary key ve otomatik artan kolon olmasini sagladik.
+        public int Id { get; set; }
+
+        [Required]  //Bos gecilemez demektir.
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public DateTime ModifiedOn { get; set; }
+
+        [Required,StringLength(30)] //kullanici adini 30 karakterler sinirladik.
+        public string ModifiedUsername { get; set; }
+    }
+}
